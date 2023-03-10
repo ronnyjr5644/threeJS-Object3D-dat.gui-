da440f86eb63393216308bd6ded9cff25dd09484
Raj <c-rraj2@dovercorp.com>
2023-03-10 15:14:01 +0530

    first commit

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,77 +1,76 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import stats from 'three/examples/jsm/libs/stats.module'
+import {GUI} from 'dat.gui'
+
 const scene = new THREE.Scene()
-const scene2 = new THREE.Scene()
-scene.background=new THREE.Color(0x448800)
-const camera1 = new THREE.PerspectiveCamera(
+scene.add(new THREE.AxesHelper(5))
+
+
+const camera = new THREE.PerspectiveCamera(
     75,
-    1,
+    window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
-const camera2=new THREE.OrthographicCamera(-1,1,1,-1,.1,10)
-const camera3=new THREE.OrthographicCamera(-1,1,1,-1,.1,10)
-const camera4=new THREE.OrthographicCamera(-1,1,1,-1,.1,10)
-camera1.position.z = 2
-camera2.position.y = 2
-camera2.lookAt(new THREE.Vector3())
-camera3.position.x = -2
-camera3.lookAt(new THREE.Vector3())
-camera4.position.z = 2
-const canvas1 =document.getElementById('c1') as HTMLCanvasElement
-const canvas2 =document.getElementById('c2') as HTMLCanvasElement
-const canvas3 =document.getElementById('c3') as HTMLCanvasElement
-const canvas4 =document.getElementById('c4') as HTMLCanvasElement
-const renderer1 = new THREE.WebGLRenderer({canvas:canvas1})
-renderer1.setSize(200,200)
-const renderer2 = new THREE.WebGLRenderer({canvas:canvas2})
-renderer2.setSize(200,200)
-const renderer3 = new THREE.WebGLRenderer({canvas:canvas3})
-renderer3.setSize(200,200)
-const renderer4 = new THREE.WebGLRenderer({canvas:canvas4})
-renderer4.setSize(200,200)
-//document.body.appendChild(renderer.domElement)
+camera.position.z = 2
 
-new OrbitControls(camera1, renderer1.domElement)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.TorusKnotGeometry()//THREE.BoxGeometry()
+const controls=new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change',render)
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.scale.x=.5
-cube.scale.y=.5
-cube.scale.z=.5
 scene.add(cube)
-const cube2=new THREE.Mesh(geometry,material)
-scene2.add(cube2)
 
-// window.addEventListener('resize', onWindowResize, false)
-// function onWindowResize() {
-//     camera.aspect = window.innerWidth / window.innerHeight
-//     camera.updateProjectionMatrix()
-//     renderer.setSize(window.innerWidth, window.innerHeight)
-//     render()
-// }
-
-function animate() {
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    cube2.rotation.y += 0.01
-    cube2.rotation.z += 0.01
-
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+const stat=stats()
+document.body.appendChild(stat.dom)
+const gui=new GUI()
+const cubeFolder=gui.addFolder("Cube")
+const cubeRotationFolder=cubeFolder.addFolder("Rotation")
+cubeRotationFolder.add(cube.rotation,"x",0,Math.PI*2)
+cubeRotationFolder.add(cube.rotation,"y",0,Math.PI*2)
+cubeRotationFolder.add(cube.rotation,"z",0,Math.PI*2)
+
+const cubePositionFolder=cubeFolder.addFolder("Position")
+cubePositionFolder.add(cube.position,"x",-10,10,2)
+cubePositionFolder.add(cube.position,"y",-10,10,2)
+cubePositionFolder.add(cube.position,"z",-10,10,2)
+
+const cubescaleFolder=cubeFolder.addFolder("Scale")
+cubescaleFolder.add(cube.scale,"x",-5,5)
+cubescaleFolder.add(cube.scale,"y",-5,5)
+cubescaleFolder.add(cube.scale,"z",-5,5)
+
+cubeFolder.add(cube,"visible")
+
+ function animate() {
+  requestAnimationFrame(animate)
+// stat.begin()
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
+// stat.end()
+  render()
+stat.update()
+ }
 
 function render() {
-    renderer1.render(scene, camera1)
-    renderer2.render(scene2, camera2)
-    renderer3.render(scene, camera3)
-    renderer4.render(scene, camera4)
+    renderer.render(scene, camera)
 }
 
 animate()
+// render()
